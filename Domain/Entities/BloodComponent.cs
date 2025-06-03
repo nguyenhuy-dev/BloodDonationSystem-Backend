d46ff6c7e40e10e21c8fafa6df216ab248498bd5
urjellyfish <annaphan0511@gmail.com>
2025-06-03 08:52:23 +0700

    pull login

---
+++ Domain/Entities/BloodComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class BloodComponent
+{
+    public int Id { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public virtual ICollection<Inventory> Inventories { get; set; } = new List<Inventory>();
+}
---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Entities;
+
+public partial class Event
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public int MaxOfDonor { get; set; }
+
+    public Guid CreateBy { get; set; }
+
+    public DateTime CreateAt { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public virtual User CreateByNavigation { get; set; } = null!;
+
+    public virtual ICollection<EventBloodType> EventBloodTypes { get; set; } = new List<EventBloodType>();
+
+    public virtual ICollection<Registration> Registrations { get; set; } = new List<Registration>();
+}
