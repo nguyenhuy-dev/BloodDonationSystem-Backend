c86eaacd4c55b7652360fcb8ba1f19d1b26f9d9e
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2025-07-29 11:00:51 +0700

    feat(report): get events for admin dashboard

---
+++ Application/DTO/EventsDTO/EventForDashAdminDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTO.EventsDTO
+{
+    public class EventForDashAdminDTO
+    {
+        public int Id { get; set; }
+        public DateOnly EventTime { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public int SuccessfulBloodRegisCount { get; set; }
+        public int BloodRegisCount { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public bool IsExpired { get; set; }
+        public bool IsUrgent { get; set; }
+        public string? BloodType { get; set; } = null;
+        public string? BloodComponent { get; set; } = null;
+        public int? BloodTypeId { get; set; }
+    }
+}
